a0880e3353e0e9f266a96f5a2cf6f674d98bbde0
Ali Asghar <aliasghar.chakera@gmail.com>
2023-09-24 23:41:23 +0500

    fixed 5a and b

--- test.py
+++ test.py
@@ -21,4 +21,27 @@ def test2():
         var += (math.pow(math.cos(i*math.pi/8), 2) - ans)**2
     var /= 8
     print(ans, var)
-test2()
+    
+def test3():
+    ans = 0
+    for i in range(-3, 5, 1):
+        ans += i
+    ans /= 8
+    var = 0
+    for i in range(-3, 5, 1):
+        var += (i - ans)**2
+    var /= 8
+    print(ans, var)
+    
+def test4():
+    ans = 0
+    for i in range(-3, 5, 1):
+        ans += -2*i**2 + 3
+    ans /= 8
+    var = 0
+    for i in range(-3, 5, 1):
+        var += (-2*i**2 + 3 - ans)**2
+    var /= 8
+    print(ans, var)
+    
+test4()
